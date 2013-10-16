d8d64fbbc732fd577aa4a523313cc496d37cb69d
Rahul <rahul@ubuntu.(none)>
2013-10-16 11:49:26 +0530

    exception handling

--- basicAuth.js
+++ basicAuth.js
@@ -2,38 +2,46 @@ var http = require('http');
 http.createServer(
 		function(req, res) {
 
-			var header = req.headers.authorization;
-			token = header.split(" ")[1];//
-			auth = new Buffer(token, 'base64').toString();
-			parts = auth.split(/:/);
-			username = parts[0];
-			password = parts[1];
-
-			var fs = require('fs');
-			var access = false;
-			var file = 'username.json';
 			res.writeHead(200, {
 				'Content-Type' : 'text/plain'
 			});
-			fs.readFile(file, 'utf8', function(err, data) {
-				if (err) {
-					res.end('Internal Error Occured\n');
-					return;
-				} else {
-					data = JSON.parse(data);
-					for ( var i = 0; i < data.length; i++) {
-						if (data[i].username == username
-								&& data[i].password == password)
-							access = true;
-					}
+			if (req.method == 'POST') {
+				try {
+					var header = req.headers.authorization;
+					token = header.split(" ")[1];//
+					auth = new Buffer(token, 'base64').toString();
+					parts = auth.split(/:/);
+					username = parts[0];
+					password = parts[1];
+
+					var fs = require('fs');
+					var access = false;
+					var file = 'username.json';
+					fs.readFile(file, 'utf8', function(err, data) {
+						if (err) {
+							res.end("Auth Failed\n");
+							return;
+						} else {
+							data = JSON.parse(data);
+							for ( var i = 0; i < data.length; i++) {
+								if (data[i].username == username && data[i].password == password)
+									access = true;
+							}
+						}
+
+						if (access === true)
+							res.end("Username:" + username + "\n");
+						else {
+							res.end("Auth Failed\n");
+						}
+
+					});
+				} catch (err) {
+					res.end("Auth Failed\n");
 				}
 
-				if (access === true)
-					res.end("Username:" + username + "\n");
-				else {
-					res.end("Auth Failed\n" + access);
-				}
-
-			});
+			} else {
+				res.end("Auth Failed\n");
+			}
 
 		}).listen(9900, '127.0.0.1');
